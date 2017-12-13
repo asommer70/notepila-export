b555bce3dc7488ac7524221418df8777662d0013
Adam Sommer <adam@thehoick.com>
2017-12-13 14:02:38 -0500

    Can export all notes into their respective folders.

--- export.py
+++ export.py
@@ -14,38 +14,42 @@ url = "http://{}:{}@notes:5984".format(os.environ.get("USER"), os.environ.get("P
 couch = couchdb.Server(url)
 db = couch['notepila']
 
+# Test notes.
 # doc = db['02_19_2017']
-# doc = db['local_ecommerce_traffic_webinar_notes']
-# doc = db['how_to_write_an_epic_blog_post_notes']
-doc = db['seo_notes']
-if not os.path.exists('data/' + doc['folder']):
-        os.makedirs('data/' + doc['folder'])
+# doc = db['seo_notes']
 
-note_file = open('data/' + doc['folder'] + '/' + doc['_id'] + '.md','w')
-note_file.write('# ' + doc['title'] + "\n\n")
+# Get all notes.
+for row in db.view('notes/index'):
+    doc = db[row['id']]
 
-for block in doc['body']['blocks']:
+    if not os.path.exists('data/' + doc['folder']):
+            os.makedirs('data/' + doc['folder'])
 
-    # Determine if there is a link in the text, and if so create a Markdown link for it.
-    block_text = block['text']
-    if (len(block['entityRanges']) > 0):
-        before_text = block['text'][0:block['entityRanges'][0]['offset']]
-        link_text = block['text'][block['entityRanges'][0]['offset']:block['entityRanges'][0]['offset'] + block['entityRanges'][0]['length']]
-        after_text = block['text'][block['entityRanges'][0]['offset'] + block['entityRanges'][0]['length']:]
-        link = doc['body']['entityMap'][str(block['entityRanges'][0]['key'])]['data']['url']
+    note_file = open('data/' + doc['folder'] + '/' + doc['_id'] + '.md','w')
+    note_file.write('# ' + doc['title'] + "\n\n")
 
-        block_text = before_text + '[' + link_text + '](' + link + ')' + after_text
+    for block in doc['body']['blocks']:
 
-    if (block['type'] == 'unstyled'):
-        # print(block_text)
-        note_file.write(block_text + "\n")
-    elif (block['type'] == 'ordered-list-item'):
-        # print('1. ' + block_text)
-        note_file.write('1. ' + block_text + "\n")
-    elif (block['type'] == 'unordered-list-item'):
-        # print('* ' + block_text)
-        note_file.write('* ' + block_text + "\n")
+        # Determine if there is a link in the text, and if so create a Markdown link for it.
+        block_text = block['text']
+        if (len(block['entityRanges']) > 0):
+            before_text = block['text'][0:block['entityRanges'][0]['offset']]
+            link_text = block['text'][block['entityRanges'][0]['offset']:block['entityRanges'][0]['offset'] + block['entityRanges'][0]['length']]
+            after_text = block['text'][block['entityRanges'][0]['offset'] + block['entityRanges'][0]['length']:]
+            link = doc['body']['entityMap'][str(block['entityRanges'][0]['key'])]['data']['url']
 
-note_file.write("\n---\n")
-note_file.write('<span style="font-size: 10px; color; silver;">Created in Note Pila! on: {} Last updated at: {}.</span>'.format(str(doc['createdAt']), str(doc['updatedAt'])))
-note_file.close()
+            block_text = before_text + '[' + link_text + '](' + link + ')' + after_text
+
+        if (block['type'] == 'unstyled'):
+            # print(block_text)
+            note_file.write(block_text + "\n")
+        elif (block['type'] == 'ordered-list-item'):
+            # print('1. ' + block_text)
+            note_file.write('1. ' + block_text + "\n")
+        elif (block['type'] == 'unordered-list-item'):
+            # print('* ' + block_text)
+            note_file.write('* ' + block_text + "\n")
+
+    note_file.write("\n---\n")
+    note_file.write('<span style="font-size: 10px; color; silver;">Created in Note Pila! on: {} Last updated at: {}.</span>'.format(str(doc['createdAt']), str(doc['updatedAt'])))
+    note_file.close()
